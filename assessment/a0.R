7117255d18d05d7d8440a6ee20517587e6b93c5d
Anton Angelgardt <a.n.angelgardt@gmail.com>
2021-01-04 16:19:26 +0300

    add a0 and assessment index page

---
+++ assessment/a0.R
@@ -0,0 +1,34 @@
+### A0
+
+# creating variables
+# place a cursor on the 6th row and press Ctrl + Enter (Cmd + Enter) twice
+
+x <- 1:3
+y <- 10:15
+
+
+## 6. Sum calculation
+# place a cursor on the 13th row and press Ctrl + Enter (Cmd + Enter)
+
+sum(x, y)
+
+# find the result in Console window
+
+
+## 7. Vector multiplication
+# you have two vectors --- x and y --- from lines 6 and 7
+# use multiply operator (*) to calculate its product
+# print on the 24th line x * y and press Ctrl + Enter (Cmd + Enter)
+
+# your code under this line
+
+
+# find the result in Console window and copy it to the form
+
+
+## 8. Sum again
+# print x * y inside the parentheses of sum function on the 32d line and press Ctrl + Enter (Cmd + Enter)
+
+sum()
+
+# find the result in Console window
