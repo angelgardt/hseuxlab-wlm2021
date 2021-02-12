e2500de49fc5f78215c9debaf8e34c732bc98631
Anton Angelgardt <a.n.angelgardt@gmail.com>
2021-02-13 01:12:05 +0300

    finish logic

---
+++ book/wlm2021-book/a5_script.R
@@ -0,0 +1,8 @@
+coin <- function(x) sample(c("H", "T"), x, replace = TRUE, prob = c(0.3, 0.7))
+table(coin(1000000))/1000000
+
+save.image('/Users/antonangelgardt/hseuxlab-wlm2021/assessment/coin.RData')
+coin
+
+
+142506 * 0.2^25 * 0.8^5 + 27405 * 0.2^26 * 0.8^4 + 4060 * 0.2^27 * 0.8^3 + 435 * 0.2^28 * 0.8^2 + 30 * 0.2^29 * 0.8^1 + 1 * 0.2^30 * 0.8^0
